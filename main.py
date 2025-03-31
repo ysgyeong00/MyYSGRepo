2d08b78e730fdf31d37006316db89968d054585b
ysgyeong00 <ysgyeong00@dgu.ac.kr>
2025-03-31 15:38:32 +0900

    Modify main.py at ysg

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print('Yang Seung Gyeong')
+print('hello world')  
+name='gdhong'  
+print(f'I\'m GD Hong!')  
+programming_language='Python'  
+print('Most used language is {0}'.format(programming_language))
